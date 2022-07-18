2e0219ef0d299d274e846692f47b6c94907df00f
bareera qaseem <e.bareera@gmail.com>
2022-07-18 21:39:04 +0500

    Q2 finished

--- Assignment_1_Q_2.py
+++ Assignment_1_Q_2.py
@@ -1,20 +1,39 @@
+def custom_input(input_msg):
+    while True:
+        try:
+            number = int(input(input_msg))
+            if number>0 and number<101 and not(input_msg=="Please enter your Islamiat marks: "):
+                break
+            elif number>0 and number<51 and input_msg=="Please enter your Islamiat marks: ":
+                break
+                
+            else:
+                if input_msg=="Please enter your Islamiat marks: ":
+                    print("Enter an integer between 0 and 50.")
+                else:
+                    print("Enter an integer between 0 and 100.")
+                continue
+        except:
+            if input_msg=="Please enter your Islamiat marks: ":
+                print("Enter an integer between 0 and 50.")
+            else:
+                print("Enter an integer between 0 and 100.")
+    return number
+
+
+
 name = input("Enter your name: ")
 
-i_physics_marks = input("Enter your Physics marks: ")
-i_chemistry_marks = input("Enter your Chemistry marks: ")
-i_maths_marks = input("Enter your Maths marks: ")
-i_english_marks = input("Enter your English marks: ")
-i_urdu_marks = input("Enter your Urdu marks: ")
-i_islamiat_marks = input("Enter your Islamiat marks: ")
+physics_marks = custom_input("Please enter your Physics marks: ")
+chemistry_marks = custom_input("Please enter your Chemistry marks: ")
+maths_marks = custom_input("Please enter your Maths marks: ")
+english_marks = custom_input("Please enter your English marks: ")
+urdu_marks = custom_input("Please enter your Urdu marks: ")
+islamiat_marks = custom_input("Please enter your Islamiat marks: ")
 
-physics_marks = int(i_physics_marks)
-chemistry_marks = int(i_chemistry_marks)
-maths_marks = int(i_maths_marks)
-english_marks = int(i_english_marks)
-urdu_marks =int(i_urdu_marks)
-islamiat_marks = int(i_islamiat_marks)
 
 total_marks = 100+100+100+100+100+50
+
 obtained_marks=physics_marks + chemistry_marks + maths_marks + english_marks + urdu_marks + islamiat_marks
 percentage = obtained_marks/total_marks
 print("-------------------------------------------------")
@@ -22,12 +41,12 @@ print("\nDetailed Mark Certificate HSSC-I")
 print("-------------------------------------------------")
 
 print("Name: ", name)
-print("Subject\t\tObtained Marks\t\tTotal Marks")
-print("Physics\t\t",physics_marks, "\t\t100")
-print("Chemistry\t",chemistry_marks,"\t\t100")
-print("Maths\t\t", maths_marks, "\t\t100")
-print("English\t\t", english_marks,"\t\t100")
-print("Urdu\t\t",urdu_marks,"\t\t100")
-print("Islamiat\t",islamiat_marks,"\t\t100")
-print("Total Marks\t",obtained_marks,"\t\t",total_marks)
+print("Subject\t\t Obtained Marks\t\tTotal Marks")
+print("Physics\t\t\t ",physics_marks, "\t\t 100")
+print("Chemistry\t\t ",chemistry_marks,"\t\t 100")
+print("Maths\t\t\t ", maths_marks, "\t\t 100")
+print("English\t\t\t ", english_marks,"\t\t 100")
+print("Urdu\t\t\t ",urdu_marks,"\t\t 100")
+print("Islamiat\t\t ",islamiat_marks,"\t\t 50")
+print("Total Marks\t\t",obtained_marks,"\t\t",total_marks)
 print("\nPercentage: ",percentage)
