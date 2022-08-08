9bd9b37da121a075bd71b2b1755312c17e61c278
bareera qaseem <e.bareera@gmail.com>
2022-08-09 02:11:18 +0500

    Diamond

--- Assignment_3_Q_4.py
+++ Assignment_3_Q_4.py
@@ -9,12 +9,30 @@
 #    4 4
 #     5
 
-
 for i in range(5, 0, -1):
     line = ""
     for j in range(i):
         line +=" "
     line += str(i)
+    
+    if (i<5):
+        for z in range (1, 10-2*i, 1):
+            line +=" "
+        line += str(i)
+
+
+    print(line)
+
+for i in range(2, 6):
+    line = ""
+    for j in range(i):
+        line +=" "
+    line += str(i)
+    
+    if (i<5):
+        for z in range (1, 10-2*i, 1):
+            line +=" "
+        line += str(i)
 
 
     print(line)
